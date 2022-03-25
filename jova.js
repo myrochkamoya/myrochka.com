9934f406819dfb832f8bd6bf8a07327ad40b69b8
myrochkamoya <kompok199@gmail.com>
2022-03-25 16:12:02 +0200

    init

---
+++ jova.js
@@ -0,0 +1,6 @@
+const firstName = 'сергійович'
+const lastName = 'сірка'
+const age = '3:00'
+
+console.log('ну ти пидарас блять сука  ' + firstName + ' я тобі казав шоб ти сюда не дивився ну тобі пиздец чекай мене в: ' + age)
+
